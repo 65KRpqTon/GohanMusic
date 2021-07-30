61c23f49c015f1b5d670f28c3acfa0ffd9eeb17f
65KRpqTon <88110966+65KRpqTon@users.noreply.github.com>
2021-07-31 04:14:11 +0700

    Update private.py

--- GohanMusic/private.py
+++ GohanMusic/private.py
@@ -23,11 +23,9 @@ Ketik » /help « Untuk Melihat Daftar Perintah Saya!
                   ],[
                     InlineKeyboardButton(
                          "🙎🏻‍♂ ᴀssɪsᴛᴀɴᴛ", url=f"https://t.me/{ASSISTANT_NAME}"),
-                    InlineKeyboardButton(
-                        "ᴅᴇᴠᴇʟᴏᴘᴇʀ 🧑🏻‍💻", url=f"https://t.me/{OWNER}")
                   ],[
                     InlineKeyboardButton(
-                         "✨ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ✨", url="https://github.com/Good-Boys-Exe/GohanMusic")
+                        "ᴅᴇᴠᴇʟᴏᴘᴇʀ 🧑🏻‍💻", url=f"https://t.me/{OWNER}")
                 ]
             ]
         )
